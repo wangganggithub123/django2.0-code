844121deb2a4c9c8895e24563382cb562ded4a3e
yhxt <243738794@qq.com>
2018-03-04 11:43:48 +0800

    yhxt

--- 博客阅读计数优化【app应用分离前-模型分离】-v1.0/read_statistics/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ReadNum
-
-
-class ReadNumAdmin(admin.ModelAdmin):
-    list_display=('id','read_num','content_object')
-
-admin.site.register(ReadNum,ReadNumAdmin)
--- 博客阅读计数优化【app应用分离前-模型分离】-v1.0/read_statistics/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReadStatisticsConfig(AppConfig):
-    name = 'read_statistics'
-    # verbose_name='阅读数'
