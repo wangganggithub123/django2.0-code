d39fb1ef7b222812c4ad46a761abe4590c2de19a
yhxt <243738794@qq.com>
2018-04-21 10:52:05 +0800

    yhxt
    23.html表单提交评论

---
+++ 23.html表单提交评论/yhsite/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from blog import views
+from yhsite import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),   #富文本上传图片
+    path('', views.home,name='home'),
+    path('blog/',include('blog.urls')),
+    path('login/',views.user_login,name='login'),
+    path('comment/',include('comment.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
