8f3b058224d5d044e4f403fc18ff3beacc4edcc6
yhxt <243738794@qq.com>
2018-07-04 14:56:58 +0800

    yhxt

---
+++ 27-28.获取评论数和细节处理-用所学知识实现点赞功能/yhsite/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from blog import views
+from yhsite import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),   # 富文本上传图片
+    path('', views.home,name='home'),
+    path('blog/',include('blog.urls')),                     # blog
+    path('comment/',include('comment.urls')),               # 评论
+    path('likes/',include('likes.urls')),                   # 点赞
+    path('login/',views.user_login,name='login'),
+    path('register/', views.register, name='register'),
+
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
